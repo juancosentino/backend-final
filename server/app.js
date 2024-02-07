4e083d15c224a8f4f971e6550324efca4a5ee722
unknown <juan.cosen2004@gmail.com>
2024-02-07 01:46:09 -0300

    middlewars actualizados

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 class Server{
     constructor(){
@@ -11,9 +12,8 @@ class Server{
     middlewars(){
         this.app.use(express.json())
         this.app.use(morgan('dev'))
+        this.app.use(cors('cors'))
     }
-
-
 
     routes(){
         /* middlewars - use - funciones que validan antes de la ruta
@@ -21,6 +21,9 @@ class Server{
         this.app.use('/api/products', require('../routes/products.routes'))
         this.app.use('/api/users',require('../routes/users.routes'))
         this.app.use('/api/we',require('../routes/we.routes'))
+        this.app.use('/api/prof',require('../routes/prof.routes'))
+        this.app.use('/api/pacientes',require('../routes/pacientes.routes'))
+        this.app.use('/api/turnos',require('../routes/turnos.routes'))
     }
 
     listen(){
