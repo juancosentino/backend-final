14393ec5d3b91618008c8e35715896cca51b95bb
unknown <juan.cosen2004@gmail.com>
2024-02-07 01:52:36 -0300

    agregue multer a middlewars

---
+++ middlewars/multer.js
@@ -0,0 +1,15 @@
+const multer = require('multer')
+const path = require('path')
+
+module.exports = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        let extencion = path.extname(file.originalname)
+        if(extencion !== '.jpg' && extencion !== '.png' && extencion !== '.jpeg'){
+            return cb(new Error( 'Formato Incorrecto'), false)
+        }
+
+        cb(null, true)
+    }
+})
+
